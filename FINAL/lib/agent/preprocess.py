c907a79f324581449fb6ec74583ff9645f788bab
Wen-Wei Tseng <sosiristseng@gmail.com>
2019-12-15 18:06:30 +0800

    functional approach

--- FINAL/lib/agent/preprocess.py
+++ FINAL/lib/agent/preprocess.py
@@ -13,45 +13,43 @@ __all__ = ['Transform']
 
 # transform dictionary format pass in the class
 # {'implenmented string': True}
-class Transform(object):
-    def __init__(self, preprocess_dict, device):
-        self.implemented_list = self.implenmented()
-        self.preprocess_dict = preprocess_dict
-        keys = preprocess_dict.keys()
-        for key in keys:
-            if key not in self.implemented_list:
-                raise KeyError(key, 'is not the implemented observation preprocess method.')
+class Transform():
 
+    implemented_list = ['slice_scoreboard', 'gray_scale', 'minus_observation']
+    img_height = 84
+    img_length = 84
+
+    def __init__(self, preprocess_dict, device, slice_scoreboard=True, gray_scale=True, minus_observation=True):
+        self._check_keys(preprocess_dict)
+        self.preprocess_dict = preprocess_dict
         self.device = device
-        self.transform = self._init_torchvision_method(preprocess_dict)
+        self.transform = self._init_torchvision_method()
+        self.slice_scoreboard = self._slice_scoreboard if preprocess_dict.get('slice_scoreboard') or slice_scoreboard else self._slice_scoreboard_noop
+        if preprocess_dict.get('gray_scale') or gray_scale:
+            self.gray_scale = self._gray_scale
+            self.channel = 1
+        else:
+            self.gray_scale = self._gray_scale_noop
+            self.channel = 3
+        self.minus_observation = self._minus_observation if preprocess_dict.get(
+            'minus_observation') or minus_observation else self._minus_observation_noop
 
     def __call__(self, observation, memory = None):
-        if self.preprocess_dict['slice_scoreboard'] == True:
-            observation = self._slice_scoreboard(observation)
-
-        observation = Image.fromarray(np.array(observation, dtype = np.uint8))
+        observation = Image.fromarray(observation)
         observation = self.transform(observation)
-
-        if self.preprocess_dict['gray_scale'] == True:
-            observation = self._gray_scale(observation)
-
-        if self.preprocess_dict['minus_observation'] == True:
-            observation = self._minus_observation(observation, memory)
-
+        observation = self.slice_scoreboard(observation)
+        observation = self.gray_scale(observation)
+        observation = self.minus_observation(observation, memory)
         return observation
 
     def insert_init_memory(self, observation):
-        if self.preprocess_dict['slice_scoreboard'] == True:
-            observation = self._slice_scoreboard(observation)
-
         observation = Image.fromarray(observation)
         observation = self.transform(observation)
-        if self.preprocess_dict['gray_scale'] == True:
-            observation = self._gray_scale(observation)
-
+        observation = self.slice_scoreboard(observation)
+        observation = self.gray_scale(observation)
         return observation
 
-    def _init_torchvision_method(self, preprocess_dict):
+    def _init_torchvision_method(self):
         method = [tfs.Resize((84, 84)), tfs.ToTensor()]
 
         return tfs.Compose(method)
@@ -60,26 +58,29 @@ class Transform(object):
         tensor = r * tensor[0, :, :] + g * tensor[1, :, :] + b * tensor[2, :, :]
         return tensor.unsqueeze(0)
 
+    def _gray_scale_noop(self, tensor, r = 0.2126, g = 0.7125, b = 0.0722):
+        return tensor
+
     def _minus_observation(self, observation, memory):
         if memory is None:
             raise RuntimeError("Please use agent.insert_memory() to insert initial data.")
 
         return observation.to(self.device) - memory
 
+    def _minus_observation_noop(self, observation, memory):
+        return observation
+
     def _slice_scoreboard(self, image):
-        image = image[24:, :, :]
+        return image[24:, :, :]
+
+    def _slice_scoreboard_noop(self, image):
         return image
 
     def image_size(self):
-        height = 84
-        length = 84
-        channel = 3
-        if self.preprocess_dict['gray_scale'] == True:
-            channel = 1
-        return (height, length, channel)
+        return (self.img_height, self.img_length, self.channel)
 
-    def implenmented(self):
-        implemented_list = ['slice_scoreboard', 'gray_scale', 'minus_observation']
-        return implemented_list
-
-
+    def _check_keys(self, preprocess_dict):
+        keys = preprocess_dict.keys()
+        for key in keys:
+            if key not in self.implemented_list:
+                raise KeyError(key, 'is not the implemented observation preprocess method.')
