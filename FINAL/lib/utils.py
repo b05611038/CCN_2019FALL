1fa88f1665ee1f07794054def829213fa095b701
Wen-Wei Tseng <sosiristseng@gmail.com>
2019-12-15 17:10:23 +0800

    simplify loop indexing and add logger

--- FINAL/lib/utils.py
+++ FINAL/lib/utils.py
@@ -3,6 +3,7 @@ import sys
 import json
 import yaml
 import pickle
+import logging
 
 import torch
 import torch.cuda as cuda
@@ -14,48 +15,51 @@ __all__ = ['save_object', 'load_pickle_obj', 'load_json_obj', 'load_config',
         'init_torch_device', 'Recorder']
 
 
-def save_object(fname, obj, mode = 'pickle'):
-    # the function is used to save some data in class or object in .pkl file
-    # or json file
-    if not isinstance(fname, str):
-        raise TypeError(fname, "is not string thus cannot be the file name of save.")
-
-    if mode == 'pickle':
-        if not fname.endswith('.pkl'):
+def save_pickle(fname: str, obj) -> None:
+    if not fname.endswith('.pkl'):
             fname += '.pkl'
+    with open(fname, 'wb') as f:
+        pickle.dump(obj, f)
 
-        with open(fname, 'wb') as out_file:
-            pickle.dump(obj, out_file)
 
+def save_json(fname: str, obj) -> None:
+    if not fname.endswith('.json'):
+        fname += '.json'
+    with open(fname, 'w') as f:
+        f.write(json.dumps(obj))
+
+
+def save_object(fname: str, obj, mode = 'pickle') -> None:
+    if not isinstance(fname, str):
+        raise TypeError('File name should be a string')
+    if mode == 'pickle':
+        save_pickle(fname, obj)
     elif mode == 'json':
-        if not fname.endswith('.json'):
-            fname += '.json'
-
-        obj = json.dumps(obj)
-        with open(fname, 'w') as out_file:
-            out_file.write(obj)
+        save_json(fname, obj)
     else:
-        print("WARNING: not either pickle or json mode, ignoring request.")
+        logging.warning("Neither in pickle nor json mode, ignoring request.")
 
-    return None
 
-def load_pickle_obj(fname):
+def load_pickle_obj(fname: str):
     # the function is used to read the data in .pkl file
     # if not fname.endswith('.pkl'):
         # raise RuntimeError(fname, 'is not a pickle file.')
-
+    if not isinstance(fname, str):
+        raise TypeError('File name should be a string')
     with open(fname, 'rb') as in_file:
         return pickle.load(in_file)
 
-def load_json_obj(fname):
+
+def load_json_obj(fname: str):
     # the function is used to read the data in .json file
     # if not fname.endswith('.json'):
         # raise RuntimeError(fname, 'is not a json file.')
-
+    if not isinstance(fname, str):
+        raise TypeError('File name should be a string')
     with open(fname, 'r') as in_file:
         return json.loads(in_file.read())
 
-def load_obj(fname):
+def load_obj(fname: str):
     if fname.endswith('.pkl'):
         return load_pickle_obj(fname)
     elif fname.endswith('.json'):
@@ -64,14 +68,16 @@ def load_obj(fname):
         raise RuntimeError(fname, 'is not either a json or pickle file.')
     return None
 
-def load_config(fname):
+
+def load_config(fname: str):
     with open(fname) as f:
         content = yaml.load(f, Loader=yaml.FullLoader)
     return content
 
+
 def init_torch_device(select = None):
     """
-    Selects PyTorch device. Accepts either a device object, a string ('cpu' or 'gpu'), 
+    Selects PyTorch device. Accepts either a device object, a string ('cpu' or 'gpu'),
     or a number (ID of GPU, negative number indicating use cpu)
     """
     if isinstance(select, torch.device):
@@ -93,16 +99,19 @@ def init_torch_device(select = None):
     if deviceNo >= 0:
         torch.backends.cudnn.benchmark = True
         cuda.set_device(deviceNo)
-        deviceStr = 'cuda:' + str(deviceNo)
+        deviceStr = f'cuda:{deviceNo}'
     else:
         deviceStr = 'cpu'
     device = torch.device(deviceStr)
-    print('Hardware setting done, using device:', deviceStr)
+    logging.info('Hardware setting done, using device:', deviceStr)
     return device
 
 
 class Recorder():
-    # Recoder of recording whole history in traninig.
+    """
+    Recording the whole history in training
+    """
+
     def __init__(self, record_column):
         self.record_column = record_column
         self.length_check = len(record_column)
@@ -126,36 +135,33 @@ class Recorder():
         if len(new_data) != self.length_check:
             raise IndexError('Input data length is not equal to init record length.')
 
-        insertion = []
-        for obj in new_data:
-            insertion.append(str(obj))
-
+        insertion = [str(obj) for obj in new_data]
         self.data.append(insertion)
         return None
 
-    def write(self, path, file_name, file_type = '.csv'):
-        print('Start writing recording file ...')
+    def write(self, path: str, file_name: str, file_type = '.csv'):
+        logging.info('Start writing recording file ...')
         with open(os.path.join(path, file_name) + file_type, 'w') as f:
             lines = self._build_file()
             f.writelines(lines)
-        print('Recoder writing done.')
+        logging.info('Recoder writing done.')
         return None
 
     def _build_file(self):
         lines = ['']
-        for i in range(len(self.record_column)):
+        for (i, rc) in enumerate(self.record_column):
             if i == len(self.record_column) - 1:
-                lines[0] = lines[0] + self.record_column[i] + '\n'
+                lines[0] = lines[0] + rc + '\n'
             else:
-                lines[0] = lines[0] + self.record_column[i] + ','
+                lines[0] = lines[0] + rc + ','
 
-        for i in range(len(self.data)):
+        for (i, da) in enumerate(self.data):
             new_lines = ''
-            for j in range(len(self.data[i])):
-                if j == len(self.data[i]) - 1:
-                    new_lines = new_lines + self.data[i][j] + '\n'
+            for (j, bits) in enumerate(da):
+                if j == len(da) - 1:
+                    new_lines = new_lines + bits + '\n'
                 else:
-                    new_lines = new_lines + self.data[i][j] + ','
+                    new_lines = new_lines + bits + ','
 
             lines.append(new_lines)
 
